419b1dd0877e37bbeaa9db4e6330597833204c9e
LinconCS <linconsantos@gmail.com>
2023-02-16 19:57:49 -0300

    alteracoes

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -13,3 +13,16 @@ const filme = {
 }
 
 // respostas aqui ⬇️
+
+const nomePrimeiroAtor = filme.elenco[0]
+console.log(`Nome do Primeiro Ator/Atriz:`, nomePrimeiroAtor)
+
+const nomeUltimoAtor = filme.elenco[3]
+console.log(`Nome do Último Ator/Atriz:`, nomeUltimoAtor)
+
+const todasTransmissoesHoje = filme.transmissoesHoje
+console.log(`Todas as transmissões de hoje:`, todasTransmissoesHoje)
+
+const horarioFilmeCanalBrasil = filme.transmissoesHoje[1].horario
+console.log(`Horário de transmissão do filme no Canal Brasil:`, horarioFilmeCanalBrasil)
+
